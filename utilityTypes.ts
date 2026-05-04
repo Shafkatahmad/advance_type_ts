9eed9c72d8e397c677575840696e582bed5ea1ec
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-04 14:46:52 +0600

    utility types

---
+++ utilityTypes.ts
@@ -0,0 +1,27 @@
+type Product = {
+  id: number;
+  name: string;
+  price: string;
+  stock: number;
+  color?: string
+}
+
+type ProductSummary = Pick<Product, 'id' | 'name' | 'price'>;
+
+type ProductWithOutStock = Omit<Product, 'stock' | 'color'>;
+
+type ProductWithColor = Required<Product>;
+
+const product: ProductWithColor = {
+  id: 222,
+  name: 'Mouse',
+  price: '220 tk',
+  stock: 20,
+  color: 'black'
+}
+
+type OptionalProduct = Partial<Product>
+
+type ProductReadonly = Readonly<Product>
+
+const emptyObj: Record<string, unknown> = {};
